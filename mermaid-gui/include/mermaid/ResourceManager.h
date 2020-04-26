a29929096fe59501fb634826a4dc1035b7a502df
Waldson Patrício <waldsonpatricio@gmail.com>
2020-04-26 19:08:42 -0300

    Fix ResourceManager

--- mermaid-gui/include/mermaid/ResourceManager.h
+++ mermaid-gui/include/mermaid/ResourceManager.h
@@ -16,28 +16,31 @@ class ResourceManager
     ResourceManager& operator=(const ResourceManager&) = default;
 
     template <typename... Args>
-    Resource& load(Key key, Args&&... args)
+    inline std::optional<Resource*> load(Key key, Args&&... args)
     {
         if (has(key)) {
             return get(key);
         }
 
-        std::unique_ptr<Resource> resource(new Resource(std::forward<Args>(args)...));
+        std::unique_ptr<Resource> resource(
+            Resource::create(std::forward<Args>(args)...)
+        );
+
         storage.emplace(key, std::move(resource));
 
         return get(key);
     }
 
-    std::optional<Resource&> get(Key key)
+    inline std::optional<Resource*> get(Key key)
     {
         if (!has(key)) {
             return std::nullopt;
         }
 
-        return *storage.at(key);
+        return &*storage.at(key);
     }
 
-    bool has(key Key)
+    inline bool has(Key key)
     {
         return storage.contains(key);
     }
